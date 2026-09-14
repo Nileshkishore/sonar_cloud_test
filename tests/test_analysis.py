9344298a5ee8219879466336526960805b3ea19c
Nileshkishore <nileshkishore2001@gmail.com>
2026-09-14 15:18:49 +0530

    test unit 2

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,6 +1,11 @@
+import os
 from testbook import testbook
 
-@testbook('src/analysis.ipynb', execute=True)
+NOTEBOOK_PATH = os.path.join(
+    os.path.dirname(__file__), '..', 'src', 'analysis.ipynb'
+)
+
+@testbook(NOTEBOOK_PATH, execute=True)
 def test_add(tb):
     add = tb.ref("add")
     assert add(2, 3) == 5
