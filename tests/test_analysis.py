abc5204e91793f8e3f4b5601ef1a0a7f3564b688
Nileshkishore <nileshkishore2001@gmail.com>
2026-09-14 15:31:14 +0530

    test

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -4,7 +4,7 @@ from testbook import testbook
 NOTEBOOK_PATH = os.path.join(
     os.path.dirname(__file__), '..', 'src', 'analysis.ipynb'
 )
-
+print(f"NOTEBOOK_PATH: {NOTEBOOK_PATH}")
 @testbook(NOTEBOOK_PATH, execute=True)
 def test_add(tb):
     add = tb.ref("add")
