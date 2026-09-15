a6e8d84491c169429a87fa4647b0148d23c467a1
Nileshkishore <nileshkishore2001@gmail.com>
2026-09-15 09:53:08 +0530

    test again

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,6 +1,11 @@
 
-from src import analysis
+import os
+from testbook import testbook
 
-def test_add():
-    assert analysis.add(2, 3) == 5
-    assert analysis.add(-1, 1) == 0
+NOTEBOOK_PATH = os.path.join(os.path.dirname(__file__), '..', 'src', 'analysis.ipynb')
+
+@testbook(NOTEBOOK_PATH, execute=True)
+def test_add(tb):
+    add = tb.ref('add')
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
